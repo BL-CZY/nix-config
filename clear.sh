9e2dbd4152440acb07371a1e94941a059a15dea3
BL-CZY <jiguangllsw@gmail.com>
2024-05-31 11:24:50 +0200

    w

--- clear.sh
+++ clear.sh
@@ -1,7 +1,6 @@
 nix-env --list-generations
 
-nix-collect-garbage  --delete-old
-
+nix-collect-garbage --delete-older-than 15d
 # recommeneded to sometimes run as sudo to collect additional garbage
 sudo nix-collect-garbage -d
 
